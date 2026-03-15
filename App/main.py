4962cd50724ab756fa11a70b1ae326a2f8ff4a2e
Rajat083 <155555359+Rajat083@users.noreply.github.com>
2026-03-16 01:07:44 +0530

    Modify CORS settings for allowed origins
    Updated CORS middleware to allow requests from the specified origins.

--- App/main.py
+++ App/main.py
@@ -8,20 +8,7 @@ app = FastAPI(title=settings.PROJECT_NAME)
 
 app.add_middleware(
 	CORSMiddleware,
-<<<<<<< HEAD
-	allow_origins=[
-		"http://localhost:5173",
-		"http://127.0.0.1:5173",
-		"http://localhost:4173",
-		"http://127.0.0.1:4173",
-		"http://localhost:3000",
-		"http://127.0.0.1:3000",
-	],
-	allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:\d+)?$",
-=======
-	allow_origins=["https://attendance-management-system-zzpz.vercel.app"],
-    # allow_origins=["*"],
->>>>>>> fadd85edaba66373918efe3dffe48cddb0fd7f67
+	allow_origins=["https://attendance-management-system-zzpz.vercel.app", "https://localhost:5173"],
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
